356c8079b6209cfabcf0fed488f196d1e63fb64e
홍정민 <dream7739@naver.com>
2024-03-20 17:27:36 +0900

    입 생성

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -10,6 +10,8 @@ import UIKit
 class FaceView: UIView {
 
     var scale: CGFloat = 0.90
+    var mouthCurvature: Double = 1.0 //굽은 비율 1 full smile, -1 full frown
+
     // 초기화가 완전히 완료될때까지 property에 접근 불가능
     // = 으로 bounds같은 변수나 메소드 부를 수 없음
     // computed property로 사용 필요 : 이미 저장된 값(stored property)을 계산하여 get,set
@@ -62,6 +64,26 @@ class FaceView: UIView {
         return pathForCircleCenteredAtPoint(eyeCenter, withRadius: eyeRadius)
     }
     
+    private func pathForMouth() -> UIBezierPath {
+        let mouthWidth = skullRadius / Ratios.SkullRadiusToMouthWidth
+        let mouthHeight = skullRadius / Ratios.SkullRadiusToMouthHeight
+        let mouthOffset = skullRadius / Ratios.SkulRadiusToMouthOffset
+        
+        let mouthRect = CGRect(x: skullCenter.x - mouthWidth/2, y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
+        
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height //1이면 아래쪽에 컨트롤 포인트가 놓임(height를 곱해서)
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.minY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.minY)
+        let cp1 = CGPoint(x: mouthRect.minX + mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+        let cp2 = CGPoint(x: mouthRect.maxX - mouthRect.width / 3, y: mouthRect.minY + smileOffset)
+
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = 5.0
+        return path
+    }
+    
     override func draw(_ rect: CGRect) {
         //0.0을 CGFloat형태로 변환하지 않는 이유는 리터럴(숫자값)이 주어졌을 때 인자가 CGFloat인걸 알고 자동으로 변환함
         //라디안 호보법: 2파이는 360도. 1파이는 180도
@@ -70,6 +92,7 @@ class FaceView: UIView {
         pathForCircleCenteredAtPoint(skullCenter, withRadius: skullRadius).stroke()
         pathForEye(eye: .Left).stroke()
         pathForEye(eye: .Right).stroke()
+        pathForMouth().stroke() //Bezier curve 사용 - 두 포인트 사이를 그린 선. 시작과 끝 두개의 컨트롤 포인트 필요
         
     }
 
