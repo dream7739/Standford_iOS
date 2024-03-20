de0ea5bcad49f4d6cb77bb6874cf2c7c32cb38d6
홍정민 <dream7739@naver.com>
2024-03-20 13:23:59 +0900

    얼굴 앱 프로젝트 생성

---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by 홍정민 on 2024/03/20.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
