dbd6a5ffeebbe90b6ce30111f313affd8f403725
홍정민 <dream7739@naver.com>
2024-03-20 16:58:10 +0900

    눈 생성

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -9,18 +9,68 @@ import UIKit
 
 class FaceView: UIView {
 
-
-    override func draw(_ rect: CGRect) {
-        var skullRadius = min(bounds.size.width, bounds.size.height) / 2
-        var skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+    var scale: CGFloat = 0.90
+    // 초기화가 완전히 완료될때까지 property에 접근 불가능
+    // = 으로 bounds같은 변수나 메소드 부를 수 없음
+    // computed property로 사용 필요 : 이미 저장된 값(stored property)을 계산하여 get,set
+    var skullRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    public struct Ratios {
+        static let SkullRadiusToEyeOffset: CGFloat = 3
+        static let SkullRadiusToEyeRadius: CGFloat = 10
+        static let SkullRadiusToMouthWidth: CGFloat = 1
+        static let SkullRadiusToMouthHeight: CGFloat = 3
+        static let SkulRadiusToMouthOffset: CGFloat = 3
+    }
+    
+    private enum Eye {
+        case Left
+        case Right
+    }
+    
+    private func pathForCircleCenteredAtPoint(_ midPoint: CGPoint, withRadius radius: CGFloat) -> UIBezierPath{
+        let path = UIBezierPath(
+            arcCenter: midPoint,
+            radius: radius,
+            startAngle: 0.0,
+            endAngle: CGFloat(2*M_PI),
+            clockwise: false)
+        path.lineWidth = 5.0
+        return path
+    }
+    
+    private func getEyeCenter(eye: Eye) -> CGPoint {
+        let eyeOffset = skullRadius / Ratios.SkullRadiusToEyeOffset
+        var eyeCenter = skullCenter
+        eyeCenter.y -= eyeOffset
         
+        switch eye {
+        case .Left: eyeCenter.x -= eyeOffset
+        case .Right: eyeCenter.x += eyeOffset
+        }
+        return eyeCenter
+    }
+    
+    private func pathForEye(eye: Eye) -> UIBezierPath {
+        let eyeRadius = skullRadius / Ratios.SkullRadiusToEyeRadius
+        let eyeCenter = getEyeCenter(eye: eye)
+        return pathForCircleCenteredAtPoint(eyeCenter, withRadius: eyeRadius)
+    }
+    
+    override func draw(_ rect: CGRect) {
         //0.0을 CGFloat형태로 변환하지 않는 이유는 리터럴(숫자값)이 주어졌을 때 인자가 CGFloat인걸 알고 자동으로 변환함
         //라디안 호보법: 2파이는 360도. 1파이는 180도
         //contentMode를 Redraw -> bound가 변경될때마다 다시 그림
-        let skull = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0.0, endAngle: CGFloat(2*M_PI), clockwise: false)
-        skull.lineWidth = 5.0
         UIColor.blue.set()
-        skull.stroke()
+        pathForCircleCenteredAtPoint(skullCenter, withRadius: skullRadius).stroke()
+        pathForEye(eye: .Left).stroke()
+        pathForEye(eye: .Right).stroke()
+        
     }
 
 }
