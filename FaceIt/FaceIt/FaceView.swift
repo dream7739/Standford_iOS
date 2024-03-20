0038a7f83686d768b0188cb01e8722c7b2b09819
홍정민 <dream7739@naver.com>
2024-03-20 13:54:26 +0900

    FaceView 스토리보드 연결 및 info.plist 경로 재설정

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -16,6 +16,7 @@ class FaceView: UIView {
         
         //0.0을 CGFloat형태로 변환하지 않는 이유는 리터럴(숫자값)이 주어졌을 때 인자가 CGFloat인걸 알고 자동으로 변환함
         //라디안 호보법: 2파이는 360도. 1파이는 180도
+        //contentMode를 Redraw -> bound가 변경될때마다 다시 그림
         let skull = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0.0, endAngle: CGFloat(2*M_PI), clockwise: false)
         skull.lineWidth = 5.0
         UIColor.blue.set()
