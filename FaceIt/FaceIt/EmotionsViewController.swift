3b7120575ad0c31186e2ba96b49b3fb33e1c27cc
홍정민 <dream7739@naver.com>
2024-03-24 18:06:30 +0900

    SplitViewController 생성 및 segue 연결

---
+++ FaceIt/FaceIt/EmotionsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by 홍정민 on 2024/03/24.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
